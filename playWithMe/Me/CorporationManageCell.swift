868d0f169321afcd1f5565a9ea34fb0e4bf6b526
Murray-Lee <li5322948@icloud.com>
2018-03-25 15:07:32 +0800

    add the corporation manage view

---
+++ playWithMe/Me/CorporationManageCell.swift
@@ -0,0 +1,19 @@
+//
+//  CorporationManageCell.swift
+//  playWithMe
+//
+//  Created by murray on 2018/3/21.
+//  Copyright © 2018年 Murray. All rights reserved.
+//
+
+import UIKit
+
+class CorporationManageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var stateBT: UIButton!
+    @IBOutlet weak var backCardView: UIView!
+    @IBOutlet weak var corporationImage: UIImageView!
+    @IBOutlet weak var nameLB: UILabel!
+    @IBOutlet weak var numLB: UILabel!
+    @IBOutlet weak var numImage: UIImageView!
+}
---
+++ playWithMe/Me/CorporationManageController.swift
@@ -0,0 +1,53 @@
+//
+//  CorporationManageController.swift
+//  playWithMe
+//
+//  Created by murray on 2018/3/21.
+//  Copyright © 2018年 Murray. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "corporManCell"
+
+class CorporationManageController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        var imageView = UIImageView(image: UIImage(named: "LoginImage"))
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CorporationManageCell
+        cell.setCardView(view: cell.backCardView)
+        UIButton.setButton(button: cell.stateBT)
+        cell.stateBT.backgroundColor = UIColor(red: 0/255, green: 205.0/255, blue: 102.0/255, alpha: 1.0)
+    
+        // Configure the cell
+    
+        return cell
+    }
+}
+
+extension CorporationManageController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let collectionViewSize = collectionView.frame.size
+        let collectionViewArea = Double(collectionViewSize.width * collectionViewSize.height)
+        
+        let sideSize: Double = sqrt(collectionViewArea / 8.5)
+        
+        return CGSize(width: sideSize, height: sideSize)
+    }
+}
